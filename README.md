3d7a734ae02a9f51fa1fe44d52d92beacc09f47c
kurtreyn <kurtreyn@yahoo.com>
2022-05-26 14:06:15 -0400

    basic table setup and use

--- README.md
+++ README.md
@@ -7,4 +7,43 @@
   - Each column is represented by an object in the array
 - Step 3
   - Create a table instance
-  -
+- Step 4
+  - Define the table structure using HTML
+- Step 5
+  - Use the table instance with JSX to render all necessary UI
+
+Basic Table structure
+
+```
+  return (
+    // Step 4
+    <table {...getTableProps()}>
+
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+
+
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+
+
+    </table>
+  );
+```
--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -2,6 +2,7 @@ import React, { useMemo } from 'react';
 import { useTable } from 'react-table';
 import MOCK_DATA from '../data/MOCK_DATA.json'; // Step 1
 import { COLUMNS } from './columns';
+import './table.css';
 
 function BasicTable({ tableData }) {
   // useMemo is a React hook that is similar to useEffect, but it only calls the function if one of the dependencies has changed.
@@ -15,7 +16,51 @@ function BasicTable({ tableData }) {
     data,
   });
 
-  return <div>BasicTable</div>;
+  // Step 5
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+  // getTableProps: destructure values that react table needs
+  // getTableBodyProps: destructure values that react table needs
+  // headerGroups: group of headers, every column header belongs to it's own header group
+  // on each row, prepareRow is called, which is a function that is passed to the table body
+
+  // Steps 3 & 5 can be combined to look like this:
+  // const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+  //   useTable({
+  //     columns,
+  //     data,
+  //   });
+
+  return (
+    // Step 4
+    <div className="table-component">
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default BasicTable;
