05d2ada57ecc0b827a102ae5193aace7ce645d79
kurtreyn <kurtreyn@yahoo.com>
2022-05-26 20:36:01 -0400

    created sorting table

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BasicTable from './BasicTable';
+import SortingTable from './SortingTable';
 import Table from './Table';
 
 function Child({ tableData }) {
   return (
     <div>
       <div>
-        <BasicTable tableData={tableData} />
+        {/* <BasicTable tableData={tableData} /> */}
+        <SortingTable tableData={tableData} />
       </div>
     </div>
   );
