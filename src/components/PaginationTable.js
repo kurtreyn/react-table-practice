cd8777c3f1f194e7d32d48a9ad251ebf1b1700f5
kurtreyn <kurtreyn@yahoo.com>
2022-05-27 15:52:52 -0400

    pagination: goTo page

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -22,6 +22,8 @@ function PaginationTable({ tableData }) {
     canPreviousPage,
     canNextPage,
     pageOptions,
+    gotoPage,
+    pageCount,
     state,
     prepareRow,
   } = useTable(
@@ -30,6 +32,7 @@ function PaginationTable({ tableData }) {
       // columns: columnsAlt,
       // columns: groupedColumns,
       data,
+      initialState: { pageIndex: 0 },
     },
     usePagination
   );
@@ -70,12 +73,32 @@ function PaginationTable({ tableData }) {
             {pageIndex + 1} of {pageOptions.length}
           </strong>{' '}
         </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value
+                ? Number(e.target.value) - 1
+                : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: '50px' }}
+          />
+        </span>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
         <button onClick={() => previousPage()} disabled={!canPreviousPage}>
           Previous
         </button>
         <button onClick={() => nextPage()} disabled={!canNextPage}>
           Next
         </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
       </div>
     </div>
   );
